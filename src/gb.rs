54dda05b592705a6a57a3c8796e3869dd14b7b54
JVehaun <jonathanvehaun@gmail.com>
2019-09-17 10:58:29 -0400

    Fix indices in mem_read

--- src/gb.rs
+++ src/gb.rs
@@ -6,7 +6,8 @@ pub struct VM {
     vram: [u8; 8192],
     cpu: CPU,
     cart: Cartridge,
-    regs: [u8; 0x7F],
+    regs: [u8; 0x80],
+    oam: [u8; 0xA0],
     ei: u8,
 }
 
@@ -29,21 +30,23 @@ impl VM {
     fn mem_read(&mut self, addr: u16) -> u8 {
         if addr <= 0x3FFF {        // ROM Bank
             return self.cart.rom[addr as usize];
-        } else if addr <= 0x7FFF { // ROM Bank 1-n
+        } else if addr >= 0x4000 addr <= 0x7FFF { // ROM Bank 1-n
             return self.cart.rom[addr as usize];
-        } else if addr <= 0x9FFF { // VRAM
+        } else if addr >= 0x8000 && addr <= 0x9FFF { // VRAM
             return self.vram[(addr - 0x8000) as usize];
-        } else if addr <= 0xBFFF { // Cart RAM
-            return self.cart.ram[addr as usize];
-        } else if addr <= 0xDFFF { // Low RAM
-            panic!("This mem shouldn't be accessed");
-        } else if addr <= 0xFE9F { // OAM RAM
-            panic!("This mem shouldn't be accessed");
-        } else if addr <= 0xFF7F { // I/O Registers
+        } else if addr >= 0xA000 && addr <= 0xBFFF { // Cart RAM
+            return self.cart.ram[(addr - 0xA000) as usize];
+        } else if addr >= 0xC000 && addr <= 0xDFFF { // Low RAM
+            return self.wram[(addr - 0xC000) as usize];
+        } else if addr >= 0xE000 && addr <= 0xFDFF { // Low RAM Duplicate
+            return self.wram[(addr - 0xE000) as usize];
+        } else if addr >= 0xFE00 && addr <= 0xFE9F { // OAM RAM
+            return self.oam[(addr - 0xFE00) as usize];
+        } else if addr >= 0xFF00 && addr <= 0xFF7F { // I/O Registers
             return self.regs[(addr - 0xFF00) as usize];
-        } else if addr <= 0xFFFE { // High RAM
-            panic!("This mem shouldn't be accessed");
-        } else if addr == 0xFFFF { // Interrupt Enable
+        } else if addr >= 0xFF80 && addr <= 0xFFFE { // High RAM
+            return self.wram[(addr - 0xFF80) as usize];
+        } else if addr >= 0xFFFF && addr == 0xFFFF { // Interrupt Enable
             return self.ei;
         }
 
